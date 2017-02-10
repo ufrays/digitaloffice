8d0a1f06868552f7eab0d84942ef3cb814deabfa
Dean Peng <dean.peng@sap.com>
2017-02-10 11:32:17 +0800

    change success stories.

--- webapp/controller/SuccessStories.controller.js
+++ webapp/controller/SuccessStories.controller.js
@@ -7,17 +7,30 @@ sap.ui.define([
 		onInit: function() {
 			var oComponent = this.getOwnerComponent();
 			this._router = oComponent.getRouter();
+			$.ajax("http://10.130.231.166:3000/service?key=1");
 		},
 		handleNavBack: function() {
 			this.getOwnerComponent().myNavBack();
 		},
-		
-		onPressTest1: function(){
-			$.ajax("http://10.130.231.166:3000/service?key=1");
-		},
-		
-		onPressTest2 : function(){
-			$.ajax("http://10.130.231.166:3000/service?key=2");
+
+		onPageChanged: function(oEvent) {
+			var sPageId = oEvent.getParameter("newActivePageId");
+			var sViewId = this.getView().getId();
+			switch (sPageId) {
+				case sViewId + "--" + "idImgINPEX":
+					$.ajax("http://10.130.231.166:3000/service?key=1");
+					break;
+				case sViewId + "--" + "idImgHILTI":
+					$.ajax("http://10.130.231.166:3000/service?key=2");
+					break;
+				case sViewId + "--" + "idImgPOSTNL":
+					$.ajax("http://10.130.231.166:3000/service?key=3");
+					break;
+				case sViewId + "--" + "idImgSHFA":
+					$.ajax("http://10.130.231.166:3000/service?key=4");
+					break;
+				default:
+			}
 		}
 	});
 });
