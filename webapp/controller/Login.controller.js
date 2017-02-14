df8975e645d4ed8067eb2bb009b32e0e6a3c98b7
Dean Peng <dean.peng@sap.com>
2017-02-14 09:51:04 +0800

    Adding logic control for face recognition

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -38,29 +38,59 @@ sap.ui.define([
 			}
 		},
 
-		onFaceIdentified: function(oEvent) {
-			var oData = arguments[2];
-			sap.m.MessageToast.show(JSON.stringify(oData));
-		},
-
-		onPinCodeLogin: function() {
-			var that = this;
+		_getUserModel: function(sUserId, sPinCode, sTokenId) {
 			var oUserModel = new JSONModel();
 			oUserModel.loadData("model/userProfile.json", null, false);
+			var oLoginUser = null;
 			if (oUserModel && oUserModel.getData() && oUserModel.getData().length > 0) {
-				var oLoginUser = _.find(oUserModel.getData(), function(oItem) {
-					if (oItem.pinCode === that.oFormLoginModel.getData().pinCode) {
-						return true;
-					}
-				});
+				if (!_.isEmpty(sUserId)) {
+					oLoginUser = _.find(oUserModel.getData(), function(oItem) {
+						if (oItem.id === sUserId) {
+							return true;
+						}
+					});
+				} else if (!_.isEmpty(sPinCode)) {
+					oLoginUser = _.find(oUserModel.getData(), function(oItem) {
+						if (oItem.pinCode === sPinCode) {
+							return true;
+						}
+					});
+				} else if (!_.isEmpty(sTokenId)) {
+					oLoginUser = _.find(oUserModel.getData(), function(oItem) {
+						if (oItem.photoTokenId === sTokenId) {
+							return true;
+						}
+					});
+				}
 				if (oLoginUser) {
 					var oLoginModel = new JSONModel();
 					oLoginModel.setData(oLoginUser);
-					that.getOwnerComponent().setModel(oLoginModel, "oLoginModel");
-					that._router.navTo("appointment");
-				} else {
-					that.getView().byId("idPinCode").setValueState(sap.ui.core.ValueState.Error);
+					return oLoginModel;
 				}
+			}
+		},
+
+		onFaceIdentified: function(oEvent) {
+			var oData = arguments[2];
+			this._router.navTo("appointment");
+			var oLoginModel = this._getUserModel("6", null, null);
+			if (oLoginModel) {
+				this.getOwnerComponent().setModel(oLoginModel, "oLoginModel");
+				this._router.navTo("appointment");
+			} else {
+				sap.m.MessageToast.show("Face Unknown");
+			}
+			// sap.m.MessageToast.show(JSON.stringify(oData));
+		},
+
+		onPinCodeLogin: function() {
+			var sPinCode = this.oFormLoginModel.getData().pinCode;
+			var oLoginModel = this._getUserModel(null, sPinCode, null);
+			if (oLoginModel) {
+				this.getOwnerComponent().setModel(oLoginModel, "oLoginModel");
+				this._router.navTo("appointment");
+			} else {
+				that.getView().byId("idPinCode").setValueState(sap.ui.core.ValueState.Error);
 			}
 		}
 	});
