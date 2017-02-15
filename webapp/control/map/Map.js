c87aa60da8734f68cfddfc345bb883c257b51b28
madeleine zeng <madeleine.zeng@sap.com>
2017-02-15 14:12:37 +0800

    Add debug info
    Change-Id: I5384967669cf4a24ab564ce1d54c5858c6a3ddd7

--- webapp/control/map/Map.js
+++ webapp/control/map/Map.js
@@ -65,26 +65,26 @@ sap.ui.define([
 		onBeforeRendering: function() {
 			this._sLocation = this.getProperty("location");
 			this._sDestination = this.getProperty("destination");
-			/*var sLocationInfo = "Location: " + this._sLocation + " \nDestination: " + this._sDestination;
-			sap.m.MessageToast.show(sLocationInfo, {
+			this._sLocationInfo = "Location: " + this._sLocation + " \nDestination: " + this._sDestination;
+			sap.m.MessageToast.show(this._sLocationInfo, {
 				duration: 1000,
 				width: "15em",
 				my: "center top",
 				at: "center top",
 				of: window,
 				offset: "0, 100"
-			});*/
+			});
 		},
 
 		onAfterRendering: function() {
-			/*sap.m.MessageToast.show("onAfterRendering", {
+			sap.m.MessageToast.show("onAfterRendering\n"+this._sLocationInfo, {
 				duration: 1000,
 				width: "15em",
 				my: "center top",
 				at: "center top",
 				of: window,
 				offset: "0, 200"
-			});*/
+			});
 			this._fOrientation = this.getProperty("orientation") - this._fOriOffset;
 
 			if (this._oSvg) {
